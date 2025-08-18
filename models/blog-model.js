52335403d2e42b75bacc979306cb9767c2a15f2a
Nyaigoti <osebenyaigoti@gmail.com>
2025-08-18 23:09:18 +0300

    Final

--- models/blog-model.js
+++
@@ -1,6 +0,0 @@
-const { default: mongoose } = require("mongoose");
-const blogSchema = require("../schemas/blog-schema");
-
-const Blog = mongoose.model("Blog", blogSchema)
-
-module.exports = Blog;
--- models/enquiry-model.js
+++
@@ -1,7 +0,0 @@
-const enquirySchema = require("../schemas/enquiry-schema");
-const { default: mongoose } = require("mongoose");
-
-
-const Enquiry = mongoose.model('Enquiry', enquirySchema);
-
-module.exports = Enquiry
